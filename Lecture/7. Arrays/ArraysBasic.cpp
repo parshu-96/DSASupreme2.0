aaa33ea08aa236a0ce3bfa780fa0d174d8ab2e1c
parshu-96 <parshuvmahindrakar@gmail.com>
2023-09-12 21:00:24 +0530

    Added Notes and Codes for Arrays Lecture 1

---
+++ Lecture/7. Arrays/ArraysBasic.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int arr[10];
+    // char ch[10];
+    // bool flags[223];
+    // long num[10];
+    // short num[10];
+    // cout<<"Arrays Created Successfully";
+
+    //Array Creation
+    // int arr[100];
+    // cout<<"Base Address of arr is :"<<&arr<<endl;
+    // cout<<"Base Address of arr is :"<<arr<<endl;
+    // cout<<"size of arr is :"<<sizeof(arr)<<endl;
+
+    
+    // int a=5;
+    // cout<<"Address of a is :"<<&a<<endl;
+    // cout<<"size of a is :"<<sizeof(a)<<endl;
+
+    // Array Initialization
+    // int arr[]={2,3,4,5,6};
+    // int brr[5]={1,2,3,4,5};
+    // int crr[5]={2,3};
+    // int drr[5]={1,2,3,4,5,6};   //To many elements  
+
+    int arr[5]={5,8,9,12,13};
+    cout<<arr[0]<<endl;
+    cout<<arr[3]<<endl;
+
+    return 0;
+}
